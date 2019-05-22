e00c06fa9bbb77f88957fbed723c2b45a58217c0
Cristian Caballero <crsecaballero@gmail.com>
2019-05-21 20:36:05 -0700

    Aplicando Estilos
    https://platzi.com/clases/1548-react/18697-como-aplicar-estilos3883/

--- src/components/badge.js
+++ src/components/badge.js
@@ -1,23 +1,24 @@
 import React from 'react';
 
+import "./styles/Badge.css";
 import confLogo from '../images/badge-header.svg';
 
 class Badge extends React.Component{
     render(){
         return  (
-        <div>
-            <div>
+        <div className="Badge">
+            <div className="Badge__header">
                 <img src={confLogo} alt="Logo de la Conferencia" />
             </div>
-            <div>
-                <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
+            <div className="Badge__section-name">
+                <img className="Badge__avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
                 <h1>Richard<br/> Kaufman</h1>
             </div>
-            <div>
-                <p>Frontend Engineer</p>
-                <p>@sparragus</p>
+            <div className="Badge__section-info">
+                <h3>Frontend Engineer</h3>
+                <div>@sparragus</div>
             </div>
-            <div>
+            <div className="Badge__footer">
                 #platziconf
             </div>
         </div>
