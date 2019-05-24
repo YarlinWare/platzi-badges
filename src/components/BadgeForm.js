4e9ac94f69c6b7b0d4ff018902272b43e6a316c6
Cristian Caballero <crsecaballero@gmail.com>
2019-05-23 22:23:57 -0700

    Manejo de estado
    https://platzi.com/clases/1548-react/18701-manejo-de-estado5400/

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -2,10 +2,21 @@ import React from 'react';
 
 class BadgeForm extends React.Component{
 
+    state={
+        lastName:'',
+        firstName:'',
+        email:'',
+        jobTitle:'',
+        twitter:'',
+    };
+
     handleChange=(e)=>{
-        console.log({
+        /*console.log({
             name: e.target.name,
             value: e.target.value,
+        });*/
+        this.setState({
+            [e.target.name]: e.target.value,
         });
     }
 
@@ -16,6 +27,7 @@ class BadgeForm extends React.Component{
     handleSubmit=(e)=>{
         e.preventDefault();
         console.log("Form was submitted");
+        console.log(this.state);
     }
 
     render(){
@@ -24,8 +36,24 @@ class BadgeForm extends React.Component{
                 <h1>New Attendant</h1>
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
+                        <label>Last Name</label>
+                        <input onChange={this.handleChange} className="form-control" type="text" name="lastName" value={this.state.lastName} placeholder="Last Name" />
+                    </div>
+                    <div className="form-group">
                         <label>First Name</label>
-                        <input onChange={this.handleChange} className="form-control" type="text" name="firstName" />
+                        <input onChange={this.handleChange} className="form-control" type="text" name="firstName" value={this.state.firstName} placeholder="First Name" />
+                    </div>
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input onChange={this.handleChange} className="form-control" type="email" name="email" value={this.state.email} placeholder="Email" />
+                    </div>
+                    <div className="form-group">
+                        <label>Job Title</label>
+                        <input onChange={this.handleChange} className="form-control" type="text" name="jobTitle" value={this.state.jobTitle} placeholder="Job Title" />
+                    </div>
+                    <div className="form-group">
+                        <label>Twitter</label>
+                        <input onChange={this.handleChange} className="form-control" type="text" name="twitter" value={this.state.twitter} placeholder="Twitter" />
                     </div>
                     <button onClick={this.handleClick} className="btn btn-primary" >Save</button>
                 </form>
