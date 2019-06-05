e48ae39eaa9263757ccb8164175ed65ac94904be
Cristian Caballero <crsecaballero@gmail.com>
2019-06-04 22:22:27 -0700

    added styles

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,14 +1,28 @@
 import React from 'react';
 
+import tw_logo from "../images/twitter-logo.png";
+
+import"../components/styles/BadgesList.css";
+
 class BadgesList extends React.Component{
     render(){
         return(
             <ul className="list-unstyled">
                 {this.props.badges.map((badge)=>{
                     return(
-                        <li key={badge.id}>
-                            <p>{badge.firstName} {badge.lastName}</p>
-                        </li>
+                        <li key={badge.id} className='Badge__section-name-list'>
+							<img src={badge.avatarUrl} className='Badge__avatar-list' />
+							<div>
+								{badge.firstName} {badge.lastName}
+								<br />
+								{badge.jobTitle}
+								<br />
+								<img src={tw_logo} className='tw__logo' />
+								<a href={"https://twitter.com/"+badge.twitter}>
+                                    <span className='twitter__blue_font'>{badge.twitter}</span>
+                                </a>
+							</div>
+						</li>
                     );
                 })}
             </ul>
